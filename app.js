c842e2675c2130d9b3565cc0af0955307e0c79ea
Craig Rodrigues <PagingCraig@gmail.com>
2018-03-22 08:05:21 -0400

    Rename to videoPage

--- app.js
+++ app.js
@@ -113,14 +113,14 @@ function renderVideoPage(index) {
     document.getElementById('search-bar').style.display = 'none';
     document.getElementById('search-results').style.display = 'none';
 
-    var videoPlayer = document.getElementById('video-page');
+    var videoPage = document.getElementById('video-page');
 
     var thorDescription = 'Thor: Ragnarok Darryl Short - Grandmaster Moves To Earth (2017) Jeff Goldblum Movie HD Subscribe for more official Trailers, TV Spots, Movie Clips, Featurettes and exclusive content!';
     var iframe = generateIframe('http://www.youtube.com/embed/xLvkFer6aOY');
     var homeButton = generateHomeButton();
     var videoInfo = generateVideoInfo('Title', 'Channel', thorDescription);
     
-    videoPlayer.append(iframe, videoInfo, homeButton);
+    videoPage.append(iframe, videoInfo, homeButton);
 
     addVideoPageListeners();
 }
