eae28b1238e185846b52f013ecf11bad7eef4f2b
Craig Rodrigues <PagingCraig@gmail.com>
2018-03-22 07:45:46 -0400

    Move fakeData to own file

--- app.js
+++ app.js
@@ -1,50 +1,4 @@
-var fakeData = {
-    kind: 'youtube#searchListResponse',
-    etag: '"RmznBCICv9YtgWaaa_nWDIH1_GM/mgh88STlnOM2rqKb1sCj00eacpM"',
-    nextPageToken: 'CA8QAA',
-    regionCode: 'US',
-    pageInfo: {
-        totalResults: 1000000,
-        resultsPerPage: 15
-    },
-    items: [
-        {
-            kind: 'youtube#searchResult',
-            etag: '"RmznBCICv9YtgWaaa_nWDIH1_GM/DnkS2C7QrWgr6H1vrpVGnR95aVc"',
-            id: {
-                kind: 'youtube#video',
-                videoId: 'xLvkFer6aOY'
-            },
-            snippet: {
-                publishedAt: '2018-02-16T17:30:15.000Z',
-                channelId: 'UCtnSBeKEA0swUnGHQ08SibQ',
-                title:
-                    'THOR RAGNAROK Short Film - Grandmaster Moves To Earth (2017) Jeff Goldblum Movie HD',
-                description:
-                    'Thor: Ragnarok Darryl Short - Grandmaster Moves To Earth (2017) Jeff Goldblum Movie HD Subscribe for more official Trailers, TV Spots, Movie Clips, Featurettes and exclusive content!',
-                thumbnails: {
-                    default: {
-                        url: 'https://i.ytimg.com/vi/xLvkFer6aOY/default.jpg',
-                        width: 120,
-                        height: 90
-                    },
-                    medium: {
-                        url: 'https://i.ytimg.com/vi/xLvkFer6aOY/mqdefault.jpg',
-                        width: 320,
-                        height: 180
-                    },
-                    high: {
-                        url: 'https://i.ytimg.com/vi/xLvkFer6aOY/hqdefault.jpg',
-                        width: 480,
-                        height: 360
-                    }
-                },
-                channelTitle: 'Comicbook.com',
-                liveBroadcastContent: 'none'
-            }
-        }
-    ]
-};
+'use strict';
 
 function generateSearchResultCard(index, thumbnailURL, videoTitle, videoDescription) {
     var card = document.createElement('div');
@@ -97,7 +51,7 @@ function renderHomePage() {
     
     var resultsContainer = document.getElementById('search-results');
     var video = fakeData.items[0];
-    var id = video.id.videoId;
+
     var thumbnail = video.snippet.thumbnails.medium.url;
     var title = video.snippet.title;
     var description = video.snippet.description;
@@ -138,7 +92,7 @@ function renderVideoPage(index) {
 
     iframe.width = 608;
     iframe.height = 342;
-    iframe.src = 'http://www.youtube.com/embed/' + fakeData.items[0].id.videoId;
+    iframe.src = 'http://www.youtube.com/embed/' + 'xLvkFer6aOY';
 
     homeButton.innerText = 'Back to Homepage';
     homeButton.id = 'home-button';
