51c6475d4d242118eb958eee958ba98c221dcbd2
Craig Rodrigues <craig.rodrigues@turner.com>
2018-03-28 11:02:34 -0400

    Move index out of dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: { contentBase: './dist' },
+    mode: 'production',
+    devServer: { contentBase: '.' },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
